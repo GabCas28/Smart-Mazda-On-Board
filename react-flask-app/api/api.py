16352a91053da03fad0feff5f164afec906000e7
Gabriel Castro <gabriel.castromunoz@aalto.ficd>
2020-05-04 21:10:31 +0300

    Start opf project

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,8 +1,27 @@
 import time
 from flask import Flask
+import random
 
 app = Flask(__name__)
-
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    return {
+        'time': time.strftime('%A %B, %d %Y %H:%M:%S'),
+    }
+
+@app.route('/streamData')
+def get_current_data():
+    return {
+        'speed': random.randrange(70, 150, 1),
+        'rpm': random.randrange(0, 100000,100),
+        'engineLoad': random.randrange(0, 100),
+    }
+
+
+@app.route('/processedData')
+def get_processed_data():
+    return {
+        'speed': random.randrange(0, 150),
+        'rpm': random.randrange(0, 100000,100),
+        'engineLoad': random.randrange(0, 100),
+    }
--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,26 +1,68 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
 // import Gauge from "./components/Gauge";
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("/time")
-        .then((res) => res.json())
-        .then(async (data) => {
-          setCurrentTime(data.time);
-        });
-    }, 1000);
-  });
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Gauge/> */}
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
-  );
+	const [ currentTime, setCurrentTime ] = useState(0);
+	const [ processedData, setProcessedData ] = useState({speed:0,rpm:0,engineLoad:0});
+	const [ streamData, setstreamData ] = useState({speed:0,rpm:0,engineLoad:0});
+
+	useEffect(() => {
+		const timeCall = setInterval(
+			() =>
+				fetch('/time').then((res) => res.json()).then(async (data) => {
+					setCurrentTime(data.time);
+				}),
+			1000
+		);
+		const streamDataCall = setInterval(
+			() =>
+				fetch('/streamData').then((res) => res.json()).then(async (data) => {
+					setstreamData(data);
+				}),
+			40
+		);
+		const processedDataCall = setInterval(
+			() =>
+				fetch('/processedData').then((res) => res.json()).then(async (data) => {
+					setProcessedData(data);
+				}),
+			4000
+		);
+		return () => {
+			clearInterval(timeCall);
+			clearInterval(streamDataCall);
+			clearInterval(processedDataCall);
+		};
+	}, []);
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* <Gauge/> */}
+				<p>The current time is {currentTime}.</p>
+				<table>
+					<tr>
+						<th> Type </th>
+						<th> Speed </th>
+						<th> Engine Load </th>
+						<th> RPM </th>
+					</tr>
+					<tr>
+						<td>Stream</td>
+						<td>{streamData.speed}</td>
+						<td>{streamData.engineLoad}</td>
+						<td>{streamData.rpm}</td>
+					</tr>
+					<tr>
+						<td>Average</td>
+						<td>{processedData.speed}</td>
+						<td>{processedData.engineLoad}</td>
+						<td>{processedData.rpm}</td>
+					</tr>
+				</table>
+			</header>
+		</div>
+	);
 }
 
 export default App;
