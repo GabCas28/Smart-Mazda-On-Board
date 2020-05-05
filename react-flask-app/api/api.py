f19fdb7cabe315ec546978633de8dc0e3a863109
lsoossi <juppeba@gmail.com>
2020-05-05 17:58:31 +0300

    Added comments

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -3,6 +3,7 @@ from flask import Flask
 import random
 import database
 
+# Makes a new table in the DB that is called the current date and time
 table = database.getDataBase(time.strftime('%d-%m-%Y_%H:%M:%S'))
 
 app = Flask(__name__)
@@ -23,10 +24,10 @@ def get_current_data():
 
 @app.route('/processedData')
 def get_processed_data():
+    # Updates the trip table every time this function is called
     database.updateTrip(table, time.strftime('%H:%M:%S'), random.randrange(0, 150), random.randrange(0, 100000,100), random.randrange(0, 100), random.randrange(0, 100), random.randrange(40, 120))
     return {
         'speed': random.randrange(0, 150),
         'rpm': random.randrange(0, 100000,100),
         'engineLoad': random.randrange(0, 100),
     }
-
